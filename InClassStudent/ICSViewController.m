ffe5e0a9bbe6c7317d7f865bc905e8500041b524
Brie Bunge <bbunge7@gmail.com>
2014-03-03 20:56:57 -0800

    remove unused multipeer code

--- InClassStudent/ICSViewController.m
+++ InClassStudent/ICSViewController.m
@@ -12,15 +12,6 @@
 
 @interface ICSViewController ()<MCNearbyServiceAdvertiserDelegate, MCSessionDelegate>
 
-//@property (nonatomic, strong) MCBrowserViewController *browserVC;
-//@property (nonatomic, strong) MCAdvertiserAssistant *advertiser;
-//@property (nonatomic, strong) MCSession *mySession;
-//@property (nonatomic, strong) MCPeerID *myPeerID;
-//
-//@property (nonatomic, strong) UIButton *browserButton;
-//@property (nonatomic, strong) UITextView *textBox;
-//@property (nonatomic, strong) UITextField *chatBox;
-
 @property (nonatomic, strong) MCNearbyServiceAdvertiser *advertiser;
 @property (nonatomic, strong) MCPeerID *localPeerID;
 @property (nonatomic, strong) MCPeerID *serverPeerID;
@@ -48,77 +39,6 @@
 {
     
 }
-
-/*
- - (void) setUpUI{
- //  Setup the browse button
- self.browserButton = [UIButton buttonWithType:UIButtonTypeSystem];
- [self.browserButton setTitle:@"Browse" forState:UIControlStateNormal];
- self.browserButton.frame = CGRectMake(130, 20, 60, 30);
- [self.view addSubview:self.browserButton];
- 
- //  Setup TextBox
- self.textBox = [[UITextView alloc] initWithFrame: CGRectMake(40, 150, 240, 270)];
- self.textBox.editable = NO;
- self.textBox.backgroundColor = [UIColor lightGrayColor];
- [self.view addSubview: self.textBox];
- 
- //  Setup ChatBox
- self.chatBox = [[UITextField alloc] initWithFrame: CGRectMake(40, 60, 240, 70)];
- self.chatBox.backgroundColor = [UIColor lightGrayColor];
- self.chatBox.returnKeyType = UIReturnKeySend;
- [self.view addSubview:self.chatBox];
- 
- [self.browserButton addTarget:self action:@selector(showBrowserVC) forControlEvents:UIControlEventTouchUpInside];
- }
- 
- - (void) setUpMultipeer{
- //  Setup peer ID
- self.myPeerID = [[MCPeerID alloc] initWithDisplayName:[UIDevice currentDevice].name];
- 
- //  Setup session
- self.mySession = [[MCSession alloc] initWithPeer:self.myPeerID];
- 
- //  Setup BrowserViewController
- self.browserVC = [[MCBrowserViewController alloc] initWithServiceType:@"chat" session:self.mySession];
- 
- //  Setup Advertiser
- self.advertiser = [[MCAdvertiserAssistant alloc] initWithServiceType:@"chat" discoveryInfo:nil session:self.mySession];
- 
- self.browserVC.delegate = self;
- 
- [self.advertiser start];
- }
- 
- - (void) showBrowserVC{
- [self presentViewController:self.browserVC animated:YES completion:nil];
- }
- 
- - (void) dismissBrowserVC{
- [self.browserVC dismissViewControllerAnimated:YES completion:nil];
- }
- 
- #pragma marks MCBrowserViewControllerDelegate
- 
- // Notifies the delegate, when the user taps the done button
- - (void)browserViewControllerDidFinish:(MCBrowserViewController *)browserViewController{
- [self dismissBrowserVC];
- }
- 
- // Notifies delegate that the user taps the cancel button.
- - (void)browserViewControllerWasCancelled:(MCBrowserViewController *)browserViewController{
- [self dismissBrowserVC];
- }
- 
- - (void)viewDidLoad
- {
- [super viewDidLoad];
- // Do any additional setup after loading the view, typically from a nib.
- //    [self setUpUI];
- //    [self setUpMultipeer];
- }
- */
-
 
 static NSString * const XXServiceType = @"InClass-service";
 
@@ -171,12 +91,10 @@ didReceiveInvitationFromPeer:(MCPeerID *)peerID
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
     UITouch *aTouch = [touches anyObject];
     CGPoint currentTouchPosition = [aTouch locationInView:self.view];
-//    NSLog(@"%f,%f", currentTouchPosition.x, currentTouchPosition.y);
     
     CGFloat yLoc = currentTouchPosition.y;
     CGFloat yMid = self.view.center.y;
     CGFloat per = (yLoc - yMid) / yMid;
-//    NSLog(@"yLoc %f perc %f", yLoc, per);
     
     CGFloat red = 0.5 + per;
     CGFloat green = 0.5 - per;
