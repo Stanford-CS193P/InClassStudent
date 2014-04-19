ecd1a2ba80c1e9b84fb95843ebb5eaa9466e0421
Brie Bunge <briebunge@gmail.com>
2014-04-19 16:19:23 -0700

    always on

--- InClassStudent/AppDelegate.m
+++ InClassStudent/AppDelegate.m
@@ -15,6 +15,7 @@
 {
     // Override point for customization after application launch.
     [ICSRemoteClient sharedManager];
+    [UIApplication sharedApplication].idleTimerDisabled = YES;
     return YES;
 }
 
